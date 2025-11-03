e0355e76cc8316c37264c81f14396941ef1cd105
Andy Cheng <andycfy@uchicago.edu>
2025-11-03 15:47:26 -0600

    Update run_scraper.py
    Updated the TwitterScraper import and modified the BEARER_TOKEN placeholder. Increased the max_tweets parameter in the scrape_by_username method.

--- run_scraper.py
+++ run_scraper.py
@@ -1,15 +1,7 @@
-from twitter_scraper import TwitterScraper
-
-BEARER_TOKEN = ""
-
-  # bearer token 1 ## AAAAAAAAAAAAAAAAAAAAAJNK4gEAAAAA5GRNQU3EqL15JtTzcwOJf9hMIM4%3DinRS3tLGz2fJIo0T8GDXAqTJcEHcbOLP88rpH94Jf3b70GvyCh
-  # bearer token 2 ## AAAAAAAAAAAAAAAAAAAAAGzn4gEAAAAAHUG7LIA%2FH9%2FmTPtf3ciwycfTeUc%3DXnCeyKuGAoSgZnusheP4WkQHzO5UBwv16QNJl82J52QpDTLY44
-
+from x_scraper import TwitterScraper
+BEARER_TOKEN = "enter bearer token here"
+	# bearer token 1 ## AAAAAAAAAAAAAAAAAAAAAJNK4gEAAAAA5GRNQU3EqL15JtTzcwOJf9hMIM4%3DinRS3tLGz2fJIo0T8GDXAqTJcEHcbOLP88rpH94Jf3b70GvyCh
+	# bearer token 2 ## AAAAAAAAAAAAAAAAAAAAAGzn4gEAAAAAHUG7LIA%2FH9%2FmTPtf3ciwycfTeUc%3DXnCeyKuGAoSgZnusheP4WkQHzO5UBwv16QNJl82J52QpDTLY44
 scraper = TwitterScraper(BEARER_TOKEN)
-scraper.scrape_by_username("DHSgov", max_tweets=10)
-
+scraper.scrape_by_username("DHSgov", max_tweets=80, start_time="2025-01-20T00:00:00Z")
 scraper.save_to_csv("dhs_tweets.csv")
-
-df = scraper.get_dataframe()
-if df is not None:
-    print(df)
