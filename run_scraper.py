c795613685f8a95e71a8b8883dd70006893ceb15
Andy Cheng <andycfy@uchicago.edu>
2026-01-30 14:08:29 -0600

    Update run_scraper.py
    Updated the TwitterScraper import and changed the bearer token placeholder. Modified the username and parameters for the scraping function and updated the output CSV file name.

--- run_scraper.py
+++ run_scraper.py
@@ -1,7 +1,7 @@
-from x_scraper import TwitterScraper
-BEARER_TOKEN = "[ ]" # remember to use your own bearer token
-	# bearer token 1 ## AAAAAAAAAAAAAAAAAAAAAJNK4gEAAAAA5GRNQU3EqL15JtTzcwOJf9hMIM4%3DinRS3tLGz2fJIo0T8GDXAqTJcEHcbOLP88rpH94Jf3b70GvyCh
-	# bearer token 2 ## AAAAAAAAAAAAAAAAAAAAAGzn4gEAAAAAHUG7LIA%2FH9%2FmTPtf3ciwycfTeUc%3DXnCeyKuGAoSgZnusheP4WkQHzO5UBwv16QNJl82J52QpDTLY44
+from x_scraper1_1 import TwitterScraper
+BEARER_TOKEN = "YOUR_BEARER_TOKEN_HERE" # remember to use your own bearer token
+	# bearer token 1 ## 
+	# bearer token 2 ## 
 scraper = TwitterScraper(BEARER_TOKEN)
-scraper.scrape_by_username("DHSgov", max_tweets=75, start_time="2025-01-20T00:00:00Z") # change start_time here
-scraper.save_to_csv("dhs_tweets.csv")
+scraper.scrape_by_username("elonmusk", max_tweets=100, start_time="2026-01-20T00:00:00Z") # change start_time here
+scraper.save_to_csv("scraped_tweets.csv") # change the saved .csv file name here
