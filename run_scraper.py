53c244f29bfafe11428643090f2e9b4c27400bee
Andy Cheng <andycfy@uchicago.edu>
2026-01-30 13:22:28 -0600

    Update run_scraper.py
    Updated the TwitterScraper import and modified the max_tweets parameter.

--- run_scraper.py
+++ run_scraper.py
@@ -1,7 +1,7 @@
-from twitter_scraper import TwitterScraper
-BEARER_TOKEN = "enter bearer token here"
+from x_scraper import TwitterScraper
+BEARER_TOKEN = "[ ]" # remember to use your own bearer token
 	# bearer token 1 ## AAAAAAAAAAAAAAAAAAAAAJNK4gEAAAAA5GRNQU3EqL15JtTzcwOJf9hMIM4%3DinRS3tLGz2fJIo0T8GDXAqTJcEHcbOLP88rpH94Jf3b70GvyCh
 	# bearer token 2 ## AAAAAAAAAAAAAAAAAAAAAGzn4gEAAAAAHUG7LIA%2FH9%2FmTPtf3ciwycfTeUc%3DXnCeyKuGAoSgZnusheP4WkQHzO5UBwv16QNJl82J52QpDTLY44
 scraper = TwitterScraper(BEARER_TOKEN)
-scraper.scrape_by_username("DHSgov", max_tweets=80, start_time="2025-01-20T00:00:00Z")
+scraper.scrape_by_username("DHSgov", max_tweets=75, start_time="2025-01-20T00:00:00Z") # change start_time here
 scraper.save_to_csv("dhs_tweets.csv")
